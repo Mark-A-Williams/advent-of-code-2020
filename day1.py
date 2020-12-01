b0af75bc97bde563bf653401f4ed2066ffe3054d
Mark Williams <amwilliams137@gmail.com>
2020-12-01 19:22:21 +0000

    Complete day 1

--- day1.py
+++ day1.py
@@ -1,5 +1,8 @@
 import time
 
+# Part 1
+
+start = time.time()
 targetSum = 2020
 
 with open('./inputs/1.txt') as file:
@@ -11,7 +14,6 @@ numbersSuperList = [evenNumbers, oddNumbers]
 
 class Found(Exception): pass
 
-start = time.time()
 try:
     for numberList in numbersSuperList:
         smallestValue = min(numberList)
@@ -19,8 +21,29 @@ try:
             if (n < targetSum - smallestValue):
                 for o in numberList:
                     if (n + o == targetSum):
-                        end = time.time()
                         raise Found
 except Found:
+    end = time.time()
     print('Day 1 part 1 solution: {0}'.format(n * o))
+    print('Execution took {0} ms'.format((end - start) * 1000))
+
+# Part 2
+
+start = time.time()
+
+try:
+    for counter, numberList in enumerate(numbersSuperList):
+        otherList = numbersSuperList[counter - 1]
+        smallestValueInOtherList = min(otherList)
+        for n in numberList:
+            if (n < targetSum - 2 * smallestValueInOtherList):
+                for o in otherList:
+                    # the following check took execution for this part from 15-20 ms down to 1ms!
+                    if (n + o < targetSum - smallestValueInOtherList):
+                        for p in otherList: 
+                            if (n + o + p == targetSum):
+                                raise Found
+except Found:
+    end = time.time()
+    print('Day 1 part 2 solution: {0}'.format(n * o * p))
     print('Execution took {0} ms'.format((end - start) * 1000))
