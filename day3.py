2576a86437c256586a5919f0185cc470053e9f8a
Mark Williams <mark.williams@enable.com>
2020-12-03 17:14:47 +0000

    Day 3 part 2

--- day3.py
+++ day3.py
@@ -1,5 +1,14 @@
 from executionTimer import executionTimer
 
+def numberOfTreesHit(movesRight: int, movesDown: int, pointsPerLine: int) -> int:
+    treesHit = 0
+    for (counter, line) in enumerate(lines):
+        if (counter % movesDown == 0):
+            positionToCheck = int((movesRight * counter / movesDown) % pointsPerLine)
+            if (line[positionToCheck] == '#'):
+                treesHit += 1
+    return treesHit    
+
 # Part 1
 
 timer = executionTimer()
@@ -9,11 +18,25 @@ with open('./inputs/3.txt') as file:
 
 pointsPerLine = len(lines[0])
 
-treesHit = 0
-for (counter, line) in enumerate(lines):
-    positionToCheck = (3 * counter) % pointsPerLine
-    if (line[positionToCheck] == '#'):
-        treesHit += 1
+part1Solution = numberOfTreesHit(3, 1, pointsPerLine)
+print('Day 3 part 1 solution: {0}'.format(part1Solution))
+timer.stopTimer()
 
-print(treesHit)
+# Part 2
+
+timer = executionTimer()
+
+part2SolutionComponents = [
+    numberOfTreesHit(1, 1, pointsPerLine),
+    numberOfTreesHit(3, 1, pointsPerLine),
+    numberOfTreesHit(5, 1, pointsPerLine),
+    numberOfTreesHit(7, 1, pointsPerLine),
+    numberOfTreesHit(1, 2, pointsPerLine)
+]
+
+part2Solution = 1
+for component in part2SolutionComponents:
+    part2Solution *= component
+
+print('Day 3 part 2 solution: {0}'.format(part2Solution))
 timer.stopTimer()
