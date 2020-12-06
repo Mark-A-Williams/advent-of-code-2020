5fea9add09615765c5a8bcf6e258d6fd1edba0eb
Mark Williams <amwilliams137@gmail.com>
2020-12-06 10:50:30 +0000

    Complete day 6

--- day6.py
+++ day6.py
@@ -2,14 +2,16 @@ from helpers import *
 
 def main():
     with open('./inputs/6.txt') as file:
-        groups: list[str] = [block.replace("\n", "") for block in file.read().split('\n\n')]
+        groups: list[str] = file.read().split('\n\n')
 
     # Part 1
 
     timer = ExecutionTimer()
     part1Solution = 0
     
-    for group in groups:
+    compressedGroups = [group.replace("\n", "") for group in groups]
+
+    for group in compressedGroups:
         uniqueCharacters: list[str] = []
         for char in group:
             if not (char in uniqueCharacters):
@@ -18,6 +20,36 @@ def main():
 
     print('Day 6 part 1 solution: {0}'.format(part1Solution))
     timer.stop()
+
+    # Part 2
+
+    timer = ExecutionTimer()
+    structuredGroups = [group.split("\n") for group in groups]
+
+    part2Solution = 0
+    for group in structuredGroups:
+        part2Solution += getCountForGroup(group)
+
+    print('Day 6 part 2 solution: {0}'.format(part2Solution))
+    timer.stop()
+
+def getCountForGroup(group: list[str]) -> int:
+    lettersInEveryone: list[str] = []
+    for (i, person) in enumerate(group):
+        if (i == 0):
+            for letter in person:
+                lettersInEveryone.append(letter)
+        else:
+            lettersStillInEveryone: list[str] = lettersInEveryone.copy()
+            for letter in lettersInEveryone:
+                if letter not in person:
+                    lettersStillInEveryone.remove(letter)
+                    if len(lettersStillInEveryone) == 0:
+                        return 0
+                    else:
+                        lettersInEveryone = lettersStillInEveryone.copy()
+
+    return len(lettersInEveryone)
 
 if __name__ == "__main__":
     main()
