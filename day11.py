57ee79bb37628b0878a022a339e644b6fef67231
Mark Williams <amwilliams137@gmail.com>
2020-12-12 00:54:25 +0000

    More refactor, no progress

--- day11.py
+++ day11.py
@@ -7,6 +7,7 @@ def applyNextStep(
     occupancyCountFunction: Callable[[list[list[str]]], int],
     maximumAdjacencyTolerance: int) -> tuple[bool, list[list[str]]]:
     
+    print(occupancyCountFunction.__name__)
     anyUpdated = False
     updatedSeating = copy.deepcopy(seating)
     for (i, row) in enumerate(seating):
@@ -44,25 +45,46 @@ def simpleAdjacencyCount(seating: list[list[str]], rowIndex: int, seatIndex: int
 
     return adjacentSeats.count("#")
 
-def main():
-    seating = [list(line) for line in getFileLines(11)]
+def lineOfSightCount(seating: list[list[str]], rowIndex: int, seatIndex: int) -> int:
+    return 0
 
-    # Part 1
-
-    timer = ExecutionTimer()
+def getFinalOccupiedSeats(seating: list[list[str]], callbackFunc: Callable[[list[list[str]]], int], tolerance: int) -> int:
     anyUpdated = True
     updatedSeating: list[list[str]] = []
 
     while anyUpdated:
-        anyUpdated, updatedSeating = applyNextStep(seating, simpleAdjacencyCount, 4)
-        seating = updatedSeating.copy()
+        anyUpdated, updatedSeating = applyNextStep(seating, callbackFunc, 4)
+        seating = updatedSeating
 
-    occupiedSeats = 0
+    result = 0
     for row in updatedSeating:
         for seat in row:
             if seat == "#":
-                occupiedSeats += 1
-    print(f"Day 11 part 1 solution: {occupiedSeats}")
+                result += 1
+    return result
+
+def main():
+    seating = [list(line) for line in getFileLines(11)]
+    # seating = [list(line) for line in [
+    #     "L.LL.LL.LL","LLLLLLL.LL","L.L.L..L..","LLLL.LL.LL","L.LL.LL.LL","L.LLLLL.LL","..L.L.....","LLLLLLLLLL","L.LLLLLL.L","L.LLLLL.LL"
+    # ]]
+
+    # Part 1
+
+    timer = ExecutionTimer()
+    part1Result = getFinalOccupiedSeats(copy.deepcopy(seating), simpleAdjacencyCount, 4)
+    print(f"Day 11 part 1 solution: {part1Result}")
+    timer.stop()
+
+    # Part 2
+
+    # seating = [list(line) for line in [
+    #     "L.LL.LL.LL","LLLLLLL.LL","L.L.L..L..","LLLL.LL.LL","L.LL.LL.LL","L.LLLLL.LL","..L.L.....","LLLLLLLLLL","L.LLLLLL.L","L.LLLLL.LL"
+    # ]]
+
+    timer = ExecutionTimer()
+    part2Result = getFinalOccupiedSeats(copy.deepcopy(seating), lineOfSightCount, 5)
+    print(f"Day 11 part 2 solution: {part2Result}")
     timer.stop()
 
 if __name__ == "__main__":
