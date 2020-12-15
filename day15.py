4369aee9e2caa216433b5cb5cb15196e046913b2
Mark Williams <amwilliams137@gmail.com>
2020-12-15 20:38:55 +0000

    Complete day 15

--- day15.py
+++ day15.py
@@ -1,19 +1,15 @@
 from helpers import *
 from typing import List, Dict
 
-def main():
-
+def getNthNumber(n: int):
     input = [0,20,7,16,1,18,15]
 
-    # Part 1
-
-    timer = ExecutionTimer()
     numbers: List[int] = []
     index = 0
     lastSpokenNumber = input[0]
     seenNumbers: Dict[int, List[int]] = {}
 
-    while index < 2020:
+    while index < n:
         if index < len(input):
             nextNumber = input[index]
         elif len(seenNumbers[lastSpokenNumber]) == 1:
@@ -28,9 +24,20 @@ def main():
         numbers.append(nextNumber)
         lastSpokenNumber = nextNumber
         index += 1
+    return numbers[-1]
 
-    part1Result = numbers[-1]
+
+def main():
+    # Part 1
+    timer = ExecutionTimer()
+    part1Result = getNthNumber(2020)
     print(f"Day 15 part 1 solution: {part1Result}")
+    timer.stop()
+
+    # Part 2
+    timer = ExecutionTimer()
+    part1Result = getNthNumber(30000000)
+    print(f"Day 15 part 2 solution: {part1Result}")
     timer.stop()
 
 if __name__ == "__main__":
